29aa76ed01dae8f619e56ad911e8b804a8239c36
James Henager <jameschenager@gmail.com>
2020-08-21 10:10:41 -0700

    add get and post routes to businessescontroller

---
+++ PortlandBusiness/Controllers/BusinessesController.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PortlandBusiness.Models;
+
+namespace PortlandBusiness.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BusinessesController : ControllerBase
+  {
+    private PortlandBusinessContext _db;
+
+    public BusinessesController(PortlandBusinessContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/businesses
+    [HttpGet]
+    public ActionResult<IEnumerable<Business>> Get()
+    {
+      return _db.Businesses.ToList();
+    }
+
+    // POST api/businesses
+    [HttpPost]
+    public void Post([FromBody] Business business)
+    {
+      _db.Businesses.Add(business);
+      _db.SaveChanges();
+    }
+
+  }
+}
